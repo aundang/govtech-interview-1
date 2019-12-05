de1f7168b587e710670cde29a74974f8df36d9c6
a829573 <anthony.undang@gmail.com>
2019-12-05 10:10:58 +0800

    fix: updating meaningful comments

--- getAllFiles.js
+++ getAllFiles.js
@@ -5,7 +5,7 @@ const path = require('path');
  * This function recursively returns all files from a given root directory and its subdirectories
  *
  * @param {string}  rootDir Root of the directory
- * @param {string}  arrayOfFilenames array container for the recursive call
+ * @param {string}  arrayOfFilenames array storage for returned files inside the directory
  * @return {type} Returns array of files
  */
 const getAllFiles = (rootDir, arrayOfFilenames) => {
